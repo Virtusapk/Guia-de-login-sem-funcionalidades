9465b42a9dae7cba1073cfe515dda8ed56a7c9cf
VirtusApk <https://github.com/Virtusapk/Guia-de-login-sem-funcionalidades.git>
2024-04-14 21:10:21 -0300

    source code

---
+++ painel/Logintela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.logintela;
+
+/**
+ *
+ * @author 82410835
+ */
+public class Logintela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
